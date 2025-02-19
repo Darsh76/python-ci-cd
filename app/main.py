089899d0c85eeca9c1a402402d1fa060728dcbbe
Darsh <darshmangukiya1010@gmail.com>
2025-02-19 05:57:22 +0000

    Fixed Flake8 style issues

--- app/main.py
+++ app/main.py
@@ -2,10 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello():
     return "Hello from the Python CI/CD Pipeline!"
 
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5001)  # Accessible from public IP
-
