25c9bd849bf27e6e8ec5a1217cecefefe3d4098e
Darsh <darshmangukiya1010@gmail.com>
2025-02-19 05:45:49 +0000

    Update

--- app/main.py
+++ app/main.py
@@ -7,5 +7,5 @@ def hello():
     return "Hello from the Python CI/CD Pipeline!"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)  # Accessible from public IP
+    app.run(host="0.0.0.0", port=5001)  # Accessible from public IP
 
